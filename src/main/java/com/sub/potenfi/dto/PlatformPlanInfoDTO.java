c4f248974ef5662b3a1d66fb46948f0116948799
Yeoonnii <mah7128@naver.com>
2024-12-15 04:11:28 +0900

    [refactor] 고객여정 - 구독서비스 요금제 검색 : 응답 값 수정, gitignore 수정

--- src/main/java/com/sub/potenfi/dto/PlatformPlanInfoDTO.java
+++ src/main/java/com/sub/potenfi/dto/PlatformPlanInfoDTO.java
@@ -13,7 +13,7 @@ public class PlatformPlanInfoDTO {
     private String planId;          // 요금제 Id
     private String platformId;      // 플랫폼 Id
     private String planName;        // 요금제 이름 
-    private String planFee;         // 요금제 가격
-    private String maxMembers;      // 최대 이용 가능 인원
+    private int planFee;         // 요금제 가격
+    private int maxMembers;      // 최대 이용 가능 인원
     private String isYearlyPlan;    // 연간 요금제 여부 (Y: 연간구독, N: 월구독)
 }
