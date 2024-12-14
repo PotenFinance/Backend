418b985a8324412b87aa7650bfdb64e85c8b4b34
Yeoonnii <mah7128@naver.com>
2024-12-14 20:25:19 +0900

    [feat] CORS 설정 수정

--- src/main/java/com/sub/potenfi/config/WebConfig.java
+++ src/main/java/com/sub/potenfi/config/WebConfig.java
@@ -1,5 +1,7 @@
 package com.sub.potenfi.config;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,8 +13,7 @@ public class WebConfig implements WebMvcConfigurer  {
     public void addCorsMappings(CorsRegistry registry) {
         // 모든 경로에 대해 CORS 허용
         registry.addMapping("/**")  // 모든 엔드포인트
-        .allowedOrigins("http://www.potenfinance.site")  // 허용할 출처 (클라이언트의 URL)
-        .allowedOrigins("http://localhost:3000")  // 허용할 출처 (클라이언트의 URL)
+        .allowedOrigins("http://www.potenfinance.site", "http://localhost:3000")  // 허용할 출처 (클라이언트의 URL)
         .allowedMethods("GET", "POST")  // 허용할 HTTP 메소드
         .allowedHeaders("*")  // 허용할 헤더
         .allowCredentials(true);  // 쿠키 및 인증 정보 허용
